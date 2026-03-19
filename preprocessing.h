0151a800d3615dd1fa6972344c0b6ec1435aedf8
AlbertEinstein132 <113251802+AlbertEinstein132@users.noreply.github.com>
2026-03-19 08:51:00 +0700

    Add standard library includes to preprocessing.h

--- preprocessing.h
+++ preprocessing.h
@@ -1,6 +1,13 @@
 #ifndef PREPROCESSING_H
 #define PREPROCESSING_H
-
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <unordered_set>
+#include <algorithm>
+#include <cctype>
 
 
 #endif
