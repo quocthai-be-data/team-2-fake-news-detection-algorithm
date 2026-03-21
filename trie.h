846dcf49c96cb3c7902f9180eeba025c654e1a7d
KhanhHung1507 <nguyenkhanhhung760@gmail.com>
2026-03-21 21:08:49 +0700

    Init trie struct

--- trie.h
+++ trie.h
@@ -1,4 +1,25 @@
 #ifndef TRIE_H
 #define TRIE_H
 
+#include <string>
+#include <vector>
+
+struct TrieNode {
+    TrieNode* children[26];
+    bool isEndOfWord;
+
+    TrieNode();
+};
+
+class Trie {
+public:
+    Trie();
+    void insert(std::string word);
+    bool search(std::string word);
+    int countKeywords(std::vector<std::string> words);
+
+private:
+    TrieNode* root;
+};
+
 #endif
